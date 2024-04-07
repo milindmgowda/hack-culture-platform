98bca1960ecfd6a1ae0b7f4a0b5e08342b63df39
milindmgowda <milindmgowda77@gmail.com>
2024-04-07 15:25:25 +0530

    added registration functionality

--- actions/login.ts
+++ actions/login.ts
@@ -1,6 +1,9 @@
 "use server";
 import { LoginSchema, RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
@@ -18,6 +21,25 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if (!validatedFields.success) {
         return {error : "Invalid credentials!"}
     }
+
+    const { name, email, password } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if(existingUser) {
+        return {error: "User already exists!"}
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    });
+
+    //TODO: send verifiction email
 
     return {success: "Verification link sent to your email address!"};
 }
